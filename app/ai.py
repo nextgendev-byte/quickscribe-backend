792942dd51d169ef9b20e6ad6bed67fb5cce2512
meetmakwana7396 <makwnameet7301@gmail.com>
2025-07-08 01:01:20 +0530

    created ai call api

---
+++ app/ai.py
@@ -0,0 +1,31 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+from google.genai.chats import Content
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+
+# Keep a list of previous messages
+history = []
+
+def ai(prompt):
+    # Append the new user message
+    history.append({"role": "user", "parts": [{"text": prompt}]})
+    
+    # Send entire conversation history
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[Content(**h) for h in history]
+    )
+    text = response.text
+    
+    # Append the AI's reply so it's included next time
+    history.append({"role": "model", "parts": [{"text": text}]})
+    return text
+
--- app/main.py
+++ app/main.py
@@ -1,14 +1,39 @@
-from typing import Union
-
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Form, UploadFile, File, HTTPException
+from pydantic import BaseModel
+from app.ai import ai
 
 app = FastAPI()
+
+
+class IFormData(BaseModel):
+    file: UploadFile
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/upload")
+async def get_file_input(file: UploadFile = File(...)):
+    print(file, "PURI FILE")
+
+    # Read file content
+    content = await file.read()
+
+    # Decode content
+    try:
+        text_content = content.decode("utf-8")
+        print(text_content, "BROOOOOOOOOOOOOO")
+    except UnicodeDecodeError:
+        raise HTTPException(
+            status_code=400, detail="File must be valid UTF-8 encoded text"
+        )
+    return {"file_name": f"{file.filename} is uploaded successfully!"}
+
+
+@app.post("/ai")
+async def get_ai_response(prompt: Annotated[str, Form(...)]):
+    response: str = ai(prompt)
+    return {"response": response}
